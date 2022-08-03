48a224e3efeaae10fe0f13ad81d53adc9481dbe1
shweta <shwetakodamg@gmail.com>
2022-08-03 16:43:26 +0530

    form assignment

---
+++ index.js
@@ -0,0 +1,34 @@
+  //variable assigning
+
+  let formEl = document.querySelector("form");
+  let tbodyEl = document.querySelector("tbody");
+  let tableEl = document.querySelector("table");
+
+  //adding row function
+  function onAdd(e) {
+      e.preventDefault();
+      let namee = document.getElementById("nameid").value;
+      let email = document.getElementById("email").value;
+      let phone = document.getElementById("phone").value;
+      tbodyEl.innerHTML += `
+      <tr>
+          <td>${namee}</td>
+          <td>${email}</td>
+          <td>${phone}</td>
+          <td><button class="removeBtn">Remove</button></td>
+      </tr>
+  `;
+  }
+
+  //delete the row function
+  function onDeleteRow(e) {
+      if (!e.target.classList.contains("removeBtn")) {
+          return;
+      }
+
+      let btn = e.target;
+      btn.closest("tr").remove();
+  }
+
+  formEl.addEventListener("submit", onAdd);
+  formEl.addEventListener("click", onDeleteRow);
